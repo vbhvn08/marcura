258539099fef0fe19691f865fff3f60cc0db9d4e
Vaibhav <vaibhav.nalawade@rabobank.nl>
2022-04-08 16:31:13 +0200

    feat: removes unnecessary component

--- projects/voyage/src/app/app.module.ts
+++ projects/voyage/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CostsComponent } from './components/costs/costs.component';
 import { ExpenseCategoriesComponent } from './components/expense-categories/expense-categories.component';
-import { CostItemComponent } from './components/cost-item/cost-item.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CurrencyPipe, DecimalPipe } from '@angular/common';
 import { CommentsComponent } from './components/comments/comments.component';
@@ -15,7 +14,6 @@ import { CommentsComponent } from './components/comments/comments.component';
     AppComponent,
     CostsComponent,
     ExpenseCategoriesComponent,
-    CostItemComponent,
     CommentsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
